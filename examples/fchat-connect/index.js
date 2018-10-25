254e40014d14f89e5edbcf2eaa864c0445f38c4c
Seth Logan <splogan9@gmail.com>
2018-10-24 23:05:57 -0400

    Slight syntax changes to example

--- examples/fchat-connect/index.js
+++ examples/fchat-connect/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Fchat = require('lib-fchat/fchat');
-const fchatConfig = require('./fchatConfig');
+const config = require('./fchatConfig');
 
-const fchat = new Fchat(fchatConfig);
+const fchat = new Fchat(config);
 
 fchat.connect(process.env.ACCOUNT, process.env.PASSWORD, process.env.CHARACTER)
 .then(() => {
